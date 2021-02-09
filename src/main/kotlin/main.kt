716b8566dbfb9303ef3a6d5083ab8565dea0420b
PrynsTag <201812311@fit.edu.ph>
2021-02-10 05:28:46 +0800

    add search functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,28 @@
 class ParkingLot {
-    private lateinit var parkSpace: MutableList<String?>
-    fun isSpaceInitialized() = ::parkSpace.isInitialized
+    private lateinit var parkingSlot: MutableMap<Int, Array<String>>
+    private var slotNumber = 0
+
+    fun isSpaceInitialized() = ::parkingSlot.isInitialized
+
     fun createSpace(size: Int) {
+        parkingSlot = mutableMapOf()
+        slotNumber = 0
         if (size > 0) {
+            repeat(size) {
+                slotNumber++
+                parkingSlot[slotNumber] = arrayOf("", "")
+            }
             println("Created a parking lot with $size spots.")
-            Array<String?>(size) { null }.toMutableList().also { parkSpace = it }
         } else {
             println("$size is not a valid number..")
         }
     }
 
     fun park(plateNumber: String, color: String) {
-        for (i in 0..parkSpace.lastIndex) {
-            if (parkSpace[i].isNullOrEmpty()) {
-                println("$color car parked in spot ${i+1}.")
-                parkSpace[i] = "$plateNumber $color"
+        parkingSlot.forEach { (key, value) ->
+            if (value[0] == "") {
+                parkingSlot[key] = arrayOf(plateNumber, color)
+                println("$color car parked in spot $key.")
                 return
             }
         }
@@ -22,22 +30,61 @@ class ParkingLot {
     }
 
     fun status() {
-        if (parkSpace.filterNotNull().count() > 0) {
-            parkSpace.forEachIndexed { index, element ->
-                if (element != null) println("${index+1} $element")
-            }
-        } else {
-            println("Parking lot is empty.")
-        }
+        val notNullable = parkingSlot.filterValues { it[0] != "" }
+        val countSpace = notNullable.count()
+
+        if (countSpace > 0) notNullable.forEach { (key, value) -> println("$key ${value[0]}") }
+        else println("Parking lot is empty.")
     }
 
     fun leave(spotNum: Int) {
-        if (spotNum-1 in parkSpace.indices && parkSpace[spotNum - 1] != null) {
+        if (spotNum in parkingSlot.keys) {
+            parkingSlot[spotNum] = arrayOf("", "")
             println("Spot $spotNum is free.")
-            parkSpace[spotNum - 1] = null
         } else {
             println("Space $spotNum does not exist.")
         }
+    }
+
+    fun regByColor(color: String) {
+        parkingSlot.forEach { (_, value) ->
+            if (color.equals(value[1], ignoreCase = true)) {
+                println(parkingSlot
+                        .filterValues { it[1].equals(color, ignoreCase = true) }
+                        .values
+                        .joinToString(", ") { it[0] }
+                )
+                return
+            }
+        }
+        println("No cars with color $color were found.")
+    }
+
+    fun spotByColor(color: String) {
+        parkingSlot.forEach { (_, value) ->
+            if (color.equals(value[1], ignoreCase = true)) {
+                println(parkingSlot
+                        .filterValues { it[1].equals(color, ignoreCase = true) }
+                        .keys
+                        .joinToString(", ")
+                )
+                return
+            }
+        }
+        println("No cars with color $color were found.")
+    }
+
+    fun spotByReg(plateNumber: String) {
+        parkingSlot.forEach { (_, value) ->
+            if (plateNumber == value[0]) {
+                println(parkingSlot
+                        .filterValues { it[0] == plateNumber }
+                        .keys
+                        .joinToString(", "))
+                return
+            }
+        }
+        println("No cars with registration number $plateNumber were found.")
     }
 }
 
@@ -52,6 +99,9 @@ fun main() {
                 "create" -> parking.createSpace(command[1].toInt())
                 "park" -> parking.park(command[1], command[2])
                 "status" -> parking.status()
+                "reg_by_color" -> parking.regByColor(command[1])
+                "spot_by_color" -> parking.spotByColor(command[1])
+                "spot_by_reg" -> parking.spotByReg(command[1])
                 "leave" -> parking.leave(command[1].toInt())
                 "exit" -> return
             }
